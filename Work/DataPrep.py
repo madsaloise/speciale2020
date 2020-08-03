019c65ba8125e1f5a4f629a7c3100c161d7dbf70
Mads Chrøis <32795834+Aloise3@users.noreply.github.com>
2020-08-03 14:37:25 +0200

    Update names in DataPrep

--- Work/DataPrep.py
+++ Work/DataPrep.py
@@ -5,18 +5,18 @@ import numpy as np
 import pandas as pd
 
 #Importerer
-def ImportExcelFile(Kolonner, Rækker, dataframe):
+def ImportExcelFile(decknames, winrates, dataframe):
     df = pd.read_excel (r'C:\Users\Mads\Desktop\Speciale\Kode\Git\Data\Winrates_Data.xlsx')
 
     column_names = list(df.columns)
 
-    if Kolonner == 1: 
+    if decknames == 1: 
         return column_names
     else:
         if dataframe == 1:
             return df
         else: 
-            if Rækker == 1:
+            if winrates == 1:
                 Row_list =[] 
                 # Iterate over each row 
                 for i in range((df.shape[0])):  
