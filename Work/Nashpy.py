32ec42793a8becac4af940b711c872dd1bd5a40f
Aloise3 <32795834+Aloise3@users.noreply.github.com>
2020-09-09 15:05:12 +0200

    Create Nashpy.py

---
+++ Work/Nashpy.py
@@ -0,0 +1,22 @@
+import nashpy as nash
+import numpy as np
+#Importerer DataPrep
+from DataPrep import ImportExcelFile 
+#Syntax:
+# ImportExcelFile(Kolonner, Rækker, dataframe) 
+# Det, som man gerne vil gemme fra funktionen angives som 1, de andre som 0
+# Vælger man flere med et vil den bare returnere kolonnenavnene 
+
+deck_names = ImportExcelFile(1,0,0)
+winrates = ImportExcelFile(0,1,0)
+data = ImportExcelFile(0,0,1)
+
+
+
+A = np.array(winrates)
+B = np.negative(A)
+rps = nash.Game(A, B)
+
+eqs = rps.support_enumeration()
+print(list(eqs))
+
