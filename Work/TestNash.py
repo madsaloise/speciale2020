b19086c966353c77e241901e1ec86928066d082e
Mads Chrøis <32795834+Aloise3@users.noreply.github.com>
2020-09-11 09:07:45 +0200

    Updates

--- Work/TestNash.py
+++ Work/TestNash.py
@@ -1,4 +1,15 @@
 import numpy as np
 import pandas as pd
-df = pd.read_excel (r'C:\speciale2020\Data\Winrates_Data_Artikel.xlsx')
-print(df)
+import string
+from sympy import symbols, Eq, solve
+from DataPrep import ImportExcelFile 
+#Syntax:
+# ImportExcelFile(Kolonner, Rækker, dataframe) 
+# Det, som man gerne vil gemme fra funktionen angives som 1, de andre som 0
+# Vælger man flere med et vil den bare returnere kolonnenavnene 
+
+deck_names = ImportExcelFile(1,0,0)
+winrates = ImportExcelFile(0,1,0)
+data = ImportExcelFile(0,0,1)
+payoffs = [[u for u in [(j/50.0)-1 for j in i]] for i in winrates]
+print(payoffs)
