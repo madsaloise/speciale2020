2fdb4357fccb8fb8b693fb9e78818a70989dcee0
Mads Chrøis <32795834+Aloise3@users.noreply.github.com>
2020-09-14 17:16:50 +0200

    Lavet frekvenser og andele

--- Work/MixedEqVSFrekvensGraf.py
+++ Work/MixedEqVSFrekvensGraf.py
@@ -1,13 +1,27 @@
 import matplotlib as plt
 import numpy as np
+def Average(lst): 
+    return sum(lst) / len(lst) 
 
 def MixedEqGraph(Vores_Nash, frekvenser):
+    #Splitter tuple
     MixedEq = list(Vores_Nash)
     MixedEq_Decks = []
     MixedEq_Winrates = []
     for a, b in MixedEq:
         MixedEq_Decks.append(a)
-        MixedEq_Winrates.append(b)
+        MixedEq_Winrates.append(b*100)
+    
+    #Omdanner frekvensdata til andele
+    NumberOfGames = []
+    count = 0
+    for i in frekvenser:
+        NumberOfGames.append(sum(i)+i[count])
+        count += 1 
+    ShareOfGames = []
+    for i in NumberOfGames:
+        ShareOfGames.append(100 * i / sum(NumberOfGames))
+
 
     
     
