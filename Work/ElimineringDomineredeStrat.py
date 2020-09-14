355b825a3eb4e6dbd6d8a55f916caaf2c2273cec
Mads Chrøis <32795834+Aloise3@users.noreply.github.com>
2020-09-14 14:26:17 +0200

    Fix Dominans
    + tilføj testmatrice til beregninger

--- Work/ElimineringDomineredeStrat.py
+++ Work/ElimineringDomineredeStrat.py
@@ -7,15 +7,13 @@ def ElimineringDomStrat(deck_names, winrates):
     DomineredeStrat = []
     P2Winrates = np.array(winrates)
     for j in range(len(deck_names)-1):
+        PlusEn = 0
         for i in winrates[:-1]:
-            PlusEn = count+1
-            if all(P2Winrates[count,:] <= P2Winrates[PlusEn,:]) and any(P2Winrates[count,:] < P2Winrates[PlusEn,:]):
+            if all(P2Winrates[count,:] <= P2Winrates[PlusEn,:]) and any(P2Winrates[count,:] < P2Winrates[PlusEn,:]) == True:
                 DomineredeStrat.append(count)
-                print("Række" + str(count) + "er svagt domineret af" + str(PlusEn))
+                print("Række " + str(count) + " er svagt domineret af " + str(PlusEn))
             PlusEn += 1
         count += 1
     if not DomineredeStrat:
         print("Ingen svagt dominerede strategier")
-
-
-
+        
--- Work/SpilHeleLortet.py
+++ Work/SpilHeleLortet.py
@@ -1,8 +1,6 @@
 #Imports
-import seaborn as sns
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from scipy.optimize import linprog
 
 
@@ -11,7 +9,7 @@ from DataPrep import ImportExcelFile
 #Syntax:
 # ImportExcelFile(Kolonner, Rækker, dataframe) 
 # Det, som man gerne vil gemme fra funktionen angives som 1, de andre som 0
-# Vælger man flere med et vil den bare returnere kolonnenavnene 
+# Vælger man flere med et vil den bare returnere kolonnenavnene, just dont 
 
 deck_names = ImportExcelFile(1,0,0)
 winrates = ImportExcelFile(0,1,0)
@@ -21,19 +19,14 @@ data = ImportExcelFile(0,0,1)
 from ElimineringDomineredeStrat import ElimineringDomStrat
 ElimineringDomStrat(deck_names, winrates)
 
-
 #Importerer Nash
+#Syntax: solvemixednash(decks, winrates)
 from MixedEquilibriumWinrates import solvemixednash
-#Syntax:
-# solvemixednash(decks, winrates)
-# decks = kolonnenavne
-# winrates = rækkenavne (rækkenavne er en liste over winrates, print den for at tjekke den. Misledende navn..)
-WinRatesMixedEq = solvemixednash(deck_names, winrates)
+#Printer
+print("Optimal sammensætning af deck i et mixed-nash equilibrium er: " + str(solvemixednash(deck_names, winrates)) + ". Andre decks spilles med en sandsynlighed på 0.")
 
-#Mixed Nash Equilibrium
-print("Optimal sammensætning af deck i et mixed-nash equilibrium er: " + str(WinRatesMixedEq))
-
-#Level-K Model, syntax: levelksolve(decks, winrates, levels), level 0 antages at spille uniformt. For k spillere skrives levels som k-1.
+#Level-K Model, 
+#Syntax: levelksolve(decks, winrates, levels), level 0 antages at spille uniformt. For k spillere skrives levels som k-1.
 from LevelKModelTeori import levelksolve
 print(list(levelksolve(deck_names, winrates, 4)))
 '''
