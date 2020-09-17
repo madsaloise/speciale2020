a4d7ede6c708ceaead846163af8a5a80eff84f7f
Mads Chrøis <32795834+Aloise3@users.noreply.github.com>
2020-09-17 12:37:31 +0200

    Mere CH

---
+++ Tests/def level_k_probabilities_1.py
@@ -0,0 +1,37 @@
+def level_k_probabilities_1(k,player,lamda=0.36):
+    n=len(normal_form_game_player)
+    if k == 0:
+        probs=[]
+        for i in range(n):
+            probs.append((1.0/n))
+        probs=tuple(probs)
+        return probs
+    if player == 0:
+        sum_action=[]
+        for i in range(n):
+            temp_sum = 0
+            for j in range(n):
+                temp_sum = temp_sum + level_k_probabilities_1(k-1, (player+1) % 2)[j] * normal_form_game_player[i][j][0]
+            temp_sum = np.exp(lamda * temp_sum)
+            sum_action.append(temp_sum)
+        probs=[]
+        for i in range(n):
+            temp_prob = sum_action[i]/sum(sum_action)
+            probs.append(temp_prob)
+        probs=tuple(probs)
+        return probs
+    else:
+        sum_action = []
+        for i in range(n):
+            temp_sum = 0
+            for j in range(n):
+                temp_sum = temp_sum + level_k_probabilities_1(k - 1, (player + 1) % 2)[j] * \
+                           normal_form_game_player[j][i][1]
+            temp_sum = np.exp(lamda * temp_sum)
+            sum_action.append(temp_sum)
+        probs = []
+        for i in range(n):
+            temp_prob = sum_action[i] / sum(sum_action)
+            probs.append(temp_prob)
+        probs = tuple(probs)
+        return probs
--- Work/CHFraBunden.py
+++ Work/CHFraBunden.py
@@ -1,6 +1,45 @@
+import pandas as pd
 import numpy as np
+import scipy as sp
+from scipy.stats import chisquare
+import matplotlib.pyplot as plt
+from math import factorial
+from math import exp
+import scipy
+import statsmodels
 
-def CHSolve(decks, winrates, levels):
+
+def player_distribution(levels, tau):
+    def poisson_distribution(levels, tau):
+        distribution = (exp(-tau))*(tau**levels)/(factorial(levels))
+        return distribution
+    fractions = []
+    for i in levels:
+        fractions.append(poisson_distribution(tau, i))
+    return fractions
+
+        
+def CHDistr(decks, winrates, levels, tau):
+    n = len(winrates)
+    def player_beliefs(levels):
+        fractions = player_distribution(levels, tau)
+        belief = sum(fractions[0:(levels)])
+        return belief
+    Deck_combination = []
+    Deck_temp =[]
+    for o in range(levels):
+        for i in range(n):
+            temp_dist = 0 
+            for j in range(n):
+                temp_dist = temp_dist + player_distribution(o, tau)[o-1]/player_beliefs(o)
+            temp_dist = np.exp(temp_dist)
+            Deck_temp.append(temp_dist)
+        for i in range(n):
+            temp_prob = Deck_temp[i] / sum(Deck_temp)
+            Deck_combination.append(temp_prob)
+        Deck_combination = tuple(Deck_combination)
+        
+def CHSolve(decks, winrates, levels, tau):
     num_decks=len(decks)
     #Beregner gennemsnitlige payoffs
     payoffs = [[u for u in [(j/50.0)-1 for j in i]] for i in winrates]
@@ -10,8 +49,7 @@ def CHSolve(decks, winrates, levels):
         avg_payoff.append(avg_pay)
 
     #Laver normal_form game til højere CH-Levels
-    normal_form_game = [[u for u in [[j, 100-j] for j in i]] for i in payoffs]
-
+    n = len(payoffs)
     ###LEVEL 0###
     level_0_maxpayoff = max(avg_payoff)
     level_0_index = avg_payoff.index(level_0_maxpayoff)
@@ -24,5 +62,5 @@ def CHSolve(decks, winrates, levels):
     deckID.append(np.argmin(maks_index))
     level_0_index = np.argmin(maks_index)
     maks_index = []
-    return deckID
-
+    
+    ###Level 2 til n###
