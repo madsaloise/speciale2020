5ae22f6d3e2fd74efcc87528779dbc0dff0712d5
Aloise3 <32795834+Aloise3@users.noreply.github.com>
2020-09-25 10:18:55 +0200

    Updates

--- Work/CHModelRene.py
+++ Work/CHModelRene.py
@@ -13,13 +13,13 @@ def player_distribution(tau, levels):
     for i in range(levels+1):
         truncated_fractions.append(fractions[i]/sum(fractions))
     return truncated_fractions
-
+#SSH på 1/antallet af decks, hvis det kun er lvl 0. 1 ellers
 def player_plays(winrates, level, deckID, indeks_tal):
     if level == 0:
         prob = 1/len(winrates)
         return prob
-    elif indeks_tal in deckID:
-        return 1
+    elif indeks_tal in deckID and level > 0:
+        return 1 
     else:
         return 0
 
@@ -36,27 +36,29 @@ def CHSolve(decks, winrates, levels, kommentarer, tau = 0.5):
     #Indsætter player 0's valg i en liste
     level_0_maxpayoff = max(avg_payoff)
     level_0_index = avg_payoff.index(level_0_maxpayoff)
-    #Level 1:
     deckID = [level_0_index]
 
-    #Lvl2 og up
+    #Lvl1 og op
     maks_index = []
     payoff_index = []
     deck_prob = []
     for p in range(levels):
         if p > 1:
+            #Kopierer liste
             A = list.copy(winrates)
             i_list = []
             count1 = 0
             #Beregner sandsynlighed for at du møder et deck betinget på dit level og beliefs omkring de andres levels.
             for i in A:
                 temp_dist = 0
+                #Summerer ssh for alle levels
                 for q in range(p):
                     temp_dist = temp_dist + player_distribution(tau, p)[q] * player_plays(winrates, q, deckID, count1)
                     #print(player_plays(winrates, q, deckID, count))
                 i_list.append(temp_dist)
                 count1 += 1
             count2 = 0
+            #normaliserer
             for i in i_list:
                 deck_prob.append(i_list[count2]/sum(i_list))
                 count2 += 1
@@ -74,7 +76,9 @@ def CHSolve(decks, winrates, levels, kommentarer, tau = 0.5):
                 print("Level-" + str(p) + "--->")    
                 print("Sandsynligheden for at møde andre decks: " + str(maks_index))
                 print("Payoffs: " + str(payoff_index))
-            deckID.append(payoff_index.index(max(payoff_index)))    
+            #Tilføjer deck-indekset til deckID-listen
+            deckID.append(payoff_index.index(max(payoff_index)))   
+            #Nulstiller lister 
             maks_index = []
             payoff_index = []
             deck_prob = []
@@ -92,9 +96,8 @@ def CHSolve(decks, winrates, levels, kommentarer, tau = 0.5):
         counter += 1
     return plays
 
+tau_range = [0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 2, 3, 4, 5, 100]
+for i in tau_range:
+    print("Fordeling for tau på: " + str(i))
+    print(player_distribution(i, 10)) 
 
-for i in range (5):
-    print(player_distribution(i, 4))
-
-for i in range (5):
-    print(player_distribution(1, i))
